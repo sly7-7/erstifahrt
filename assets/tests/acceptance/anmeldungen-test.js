c5384480f51ccb543833b873b40a61a89488c381
Tobias Witt <tobias.witt@hhu.de>
2018-08-18 19:04:59 +0200

    Fixup tets

--- assets/tests/acceptance/anmeldungen-test.js
+++ assets/tests/acceptance/anmeldungen-test.js
@@ -7,27 +7,27 @@ module('Acceptance | anmeldungen', function(hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('main nav link for /anmeldungen', async function(assert) {
+  test('main nav link for /', async function(assert) {
     await visit('/');
 
     const links = findAll('.nav-link');
 
     await click(links[0]);
 
-    assert.equal(currentURL(), '/anmeldungen');
+    assert.equal(currentURL(), '/');
   });
 
-  test('visiting /anmeldungen', async function(assert) {
+  test('visiting /', async function(assert) {
     const students = server.createList('student', 10);
 
-    await visit('/anmeldungen');
+    await visit('/');
 
     const studentRows = findAll('.student-row');
 
     assert.equal(studentRows.length, students.length);
 
     assert.equal(
-      studentRows[0].textContent.trim(),
+      studentRows[0].querySelector('td:nth-of-type(2)').textContent.trim(),
       `${students[0].firstName} ${students[0].lastName}`
     );
   });
