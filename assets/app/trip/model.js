b8a9e05e6df13a078ea94242390fe8ef4c938f14
Tobias Witt <tobias.witt@hhu.de>
2018-10-16 10:22:16 +0200

    Add waiting list counter to students page

--- assets/app/trip/model.js
+++ assets/app/trip/model.js
@@ -1,7 +1,7 @@
 import Model from 'ember-data/model';
 import { attr, hasMany } from '@ember-decorators/data';
 import { computed } from '@ember-decorators/object';
-import { equal } from '@ember-decorators/object/computed';
+import { equal, filterBy } from '@ember-decorators/object/computed';
 
 export default class TripModel extends Model {
   @attr title;
@@ -12,14 +12,21 @@ export default class TripModel extends Model {
 
   @attr('number') maxStudents;
 
-  @computed('maxStudents', 'students.@each.isBooked')
+  @filterBy('students', 'isBooked', true) bookedStudents;
+
+  @computed('maxStudents', 'bookedStudents.length')
+  get countWaitingList() {
+    return Math.abs(Math.min(0, this.maxStudents - this.bookedStudents.length));
+  }
+
+  @computed('maxStudents', 'bookedStudents.length')
   get placesLeft() {
     return Math.max(0, this.maxStudents - this.students.filterBy('isBooked').length);
   }
 
-  @equal('placesLeft', 1) onePlaceLeft;
-
   @equal('placesLeft', 0) noPlacesLeft;
+
+  @equal('placesLeft', 1) onePlaceLeft;
 
   @hasMany students;
 }
